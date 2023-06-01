554bbaecbd670e8b514171c19e577de2dfad3a6f
Fosa404 <giulifossatti@gmail.com>
2023-06-01 18:37:03 -0300

    configurando database

--- project_schedule/settings.py
+++ project_schedule/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'project_schedule.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/postgres',
-        conn_max_age=600
-    )
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
