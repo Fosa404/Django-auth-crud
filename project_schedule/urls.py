5c4e9d10a4474047f96da300240a50b7cb03a8ca
Fosa404 <giulifossatti@gmail.com>
2023-06-05 16:39:12 -0300

    url static

--- project_schedule/urls.py
+++ project_schedule/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('signin/', views.signin, name="signin"),
     path('signuot/', views.signout, name='signout')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
